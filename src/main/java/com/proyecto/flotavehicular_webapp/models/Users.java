8b8350e3d53557dcf4c17304149a385c0910b156
Cristhian Marecos <cristhianmareccos2222@gmail.com>
2024-09-09 01:17:28 -0400

    Test Roles

---
+++ src/main/java/com/proyecto/flotavehicular_webapp/models/Users.java
@@ -0,0 +1,42 @@
+package com.proyecto.flotavehicular_webapp.models;
+
+import com.proyecto.flotavehicular_webapp.enums.ESTATES;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String userName;
+
+    @Column(unique = true)
+    private String userEmail;
+
+    private String userPassword;
+
+    private ESTATES userState;
+
+    @OneToOne(mappedBy = "users", cascade = CascadeType.ALL)
+    private Driver conductor;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+}
+
+
