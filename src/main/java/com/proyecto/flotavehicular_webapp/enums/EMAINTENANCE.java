741255812ba9204cb372cc0c4305f561fb129b8f
Cristhian Marecos <cristhianmareccos2222@gmail.com>
2024-09-12 17:22:06 -0400

    Adding enums

---
+++ src/main/java/com/proyecto/flotavehicular_webapp/enums/EMAINTENANCE.java
@@ -0,0 +1,6 @@
+package com.proyecto.flotavehicular_webapp.enums;
+
+// Maintenance State
+public enum EMAINTENANCE {
+    REPAIR, MAINTENANCE
+}
---
+++ src/main/java/com/proyecto/flotavehicular_webapp/enums/ESTATES.java
@@ -0,0 +1,6 @@
+package com.proyecto.flotavehicular_webapp.enums;
+
+// Car States
+public enum ESTATES {
+    ACTIVE, INACTIVE
+}
